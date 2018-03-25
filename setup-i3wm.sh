0b6ba8e461de38ce68d42b0e55c759133d1c2747
aacedo <aacedo@funiglobal.com>
2018-03-25 18:13:37 +0200

    Added Gnome Calculator package

--- setup-i3wm.sh
+++ setup-i3wm.sh
@@ -39,7 +39,7 @@ if [ "$1" == "-i" ]; then
         { echo "Aborting installation..."; exit 1; }
 
     echo "Checking needed packages..."
-    check_and_install_packages i3-wm feh conky thunar dunst dmenu i3lock xbacklight scrot hibernate gimp hamster-applet clusterssh meld
+    check_and_install_packages i3-wm feh conky thunar dunst dmenu i3lock xbacklight scrot hibernate gimp hamster-applet clusterssh meld gnome-calculator
 
     echo "Backing up your files..."
     do_backup $I3STATUSCONF $CONKYRC $XINITRC $I3WM_FOLDER || \
